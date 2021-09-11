e76d5ceaa76e8281f7f24cbe73a21ab15196254c
Stephan Seitz <stephan.seitz@fau.de>
2021-09-11 18:12:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+tree-sitter-glsl
+==================
+
+This is a extension of [tree-sitter-c](https://github.com/tree-sitter/tree-sitter-c) to support
+the syntax of GLSL (https://www.khronos.org/opengl/wiki/Core_Language_(GLSL)).
---
+++ grammar.js
@@ -0,0 +1,39 @@
+const C = require("tree-sitter-c/grammar")
+
+module.exports = grammar(C, {
+    name: 'glsl',
+
+    rules: {
+        _top_level_item: (_, original) => original,
+
+        declaration: ($, original) =>
+            seq(
+                repeat(
+                    choice(
+                        'in',
+                        'out',
+                        'inout',
+                        'uniform',
+                        $.layout_specification,
+                    )
+                ), choice(seq($.identifier, $.field_declaration_list, optional($.identifier), ";"), original)
+            ),
+
+        parameter_declaration: ($, original) =>
+            seq(
+                repeat(
+                    choice(
+                        'in',
+                        'out',
+                        'inout',
+                        'uniform',
+                        $.layout_specification,
+                    )
+                ), original
+            ),
+
+        layout_specification: ($) => seq("layout", $.layout_qualifiers),
+        layout_qualifiers: ($) => seq("(", $.qualifier, repeat(seq(",", $.qualifier)), ")"),
+        qualifier: ($) => choice("push_constant", seq($.identifier, "=", $._expression)),
+    }
+});
