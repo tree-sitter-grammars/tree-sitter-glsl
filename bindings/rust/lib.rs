01376744f1baca54479c571c04c06cc3db5ddc2f
Stephan Seitz <sseitz@nvidia.com>
2025-03-16 17:39:54 +0100

    chore!: use `tree_sitter_language` crate and Rust 2024

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -1,32 +1,32 @@
 //! This crate provides glsl language support for the [tree-sitter][] parsing library.
 //!
-//! Typically, you will use the [language][language func] function to add this language to a
+//! Typically, you will use the [LANGUAGE_GLSL][] constant to add this language to a
 //! tree-sitter [Parser][], and then use the parser to parse some code:
 //!
 //! ```
+//! use tree_sitter_glsl::LANGUAGE_GLSL;
+//!
 //! let code = "";
 //! let mut parser = tree_sitter::Parser::new();
-//! parser.set_language(&tree_sitter_glsl::language()).expect("Error loading glsl grammar");
+//! parser
+//!     .set_language(&LANGUAGE_GLSL.into())
+//!     .expect("Error loading glsl language");
 //! let tree = parser.parse(code, None).unwrap();
 //! ```
 //!
-//! [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
-//! [language func]: fn.language.html
 //! [Parser]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Parser.html
 //! [tree-sitter]: https://tree-sitter.github.io/
 
-use tree_sitter::Language;
+use tree_sitter_language::LanguageFn;
 
-extern "C" {
-    fn tree_sitter_glsl() -> Language;
+unsafe extern "C" {
+    fn tree_sitter_glsl() -> *const ();
 }
 
-/// Get the tree-sitter [Language][] for this grammar.
+/// The tree-sitter [`LanguageFn`] for GLSL.
 ///
-/// [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
-pub fn language() -> Language {
-    unsafe { tree_sitter_glsl() }
-}
+/// [LanguageFn]: https://docs.rs/tree-sitter-language/*/tree_sitter_language/struct.LanguageFn.html
+pub const LANGUAGE_GLSL: LanguageFn = unsafe { LanguageFn::from_raw(tree_sitter_glsl) };
 
 /// The content of the [`node-types.json`][] file for this grammar.
 ///
@@ -42,11 +42,13 @@ pub const NODE_TYPES: &str = include_str!("../../src/node-types.json");
 
 #[cfg(test)]
 mod tests {
+    use crate::LANGUAGE_GLSL;
+
     #[test]
     fn test_can_load_grammar() {
         let mut parser = tree_sitter::Parser::new();
         parser
-            .set_language(&super::language())
-            .expect("Error loading glsl language");
+            .set_language(&LANGUAGE_GLSL.into())
+            .expect("Error loading GLSL language");
     }
 }
